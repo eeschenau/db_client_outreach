0f447d26d8b019c66180fa81c322caceb6925ac4
eeschenau <schenau.esther@gmail.com>
2017-01-28 03:37:17 -0600

    Saving soft coded plan for front UI.

---
+++ db_front.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# ------------
+# Esther Schenau
+# Start Date: 28Jan2016
+# Database Interface for user outreach.
+# ------------
+
+"""
+
+Welcome to the outreach tool! The purpose is to create an easy, actionable list of users based on their coverage for outreach purposes.
+
+"""
+
+
+#SETUP
+    #import entries in file to array which will be the WHERE clause
+    #entries should take the form of the firm's SFDC, a unique ID
+    #will hardcode for now, might be able to tie to a userID later on
+
+#FILE LOADED? Y - proceed, N - error, prompt again
+
+#REQUEST INFO
+
+    #Search by company / ticker for now. (Therapeutic area for later.)
+
+    # What company are you interested in? Enter ticker or name.
+    # SELECT clientID, clientFirm,
+    # IF nothing found, prompt user to check spelling or shorten to a commonly used version of the company name.
